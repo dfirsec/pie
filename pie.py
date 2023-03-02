b226e4df545bc5766988eb613209c840178db4f7
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2023-03-02 11:47:58 -0500

    Refactored

--- pie.py
+++ pie.py
@@ -1,42 +1,60 @@
+"""
+Takes a PDF document as an argument, extracts the text from the PDF, and then searches the text for IOCs.
+"""
 import argparse
+import contextlib
 import os
 import sys
+import unicodedata
 from pathlib import Path
+from typing import Optional
 
 import pdfplumber
 from tld import is_tld
 from tld.utils import update_tld_names
-
 from utils import Helpers, Termcolors
 
 __author__ = "DFIRSec (@pulsecode)"
-__version__ = "v0.0.8"
+__version__ = "v0.0.9"
 __description__ = "Extract Indicators of Compromise (IOCs) from PDF documents."
 
-helper = Helpers()
-tc = Termcolors()
+HELPER = Helpers()
+TC = Termcolors()
 
 # update/sync tld names
 update_tld_names()
 
 # Base directory
-parent = Path(__file__).resolve().parent
+PARENT = Path(__file__).resolve().parent
 
 
-def extractor(pdf):
+def extractor(pdf: str):
+    """
+    > If the file size is greater than 10 MB, exit the program with an error message. Otherwise, open
+    the PDF file and extract the text from each page.
+
+    :param pdf: The PDF file to be read
+    :return: A list of strings.
+    """
     size = os.path.getsize(pdf)
     large = round(size / (1024 * 1024))
     if size > 10240000:
-        sys.exit(f"{tc.RED}[ERROR]{tc.RESET} Limit file size to 10 MB or less. Your file is {large:,} MB.")
+        sys.exit(f"{TC.RED}[ERROR]{TC.RESET} Limit file size to 10 MB or less. Your file is {large:,} MB.")
     else:
         with pdfplumber.open(pdf) as pdf_file:
-            for page in pdf_file.pages:
-                yield page.extract_text()
+            return [page.extract_text() for page in pdf_file.pages if page is not None]
 
 
-def write_file(results, opt, rep):
-    if rep:
-        file_output = parent.joinpath(f"{rep.replace(' ', '_').replace('.pdf', '')}.txt")
+def write_file(results: str, opt: str, report: Optional[str]) -> None:
+    """
+    If the report is not empty, then write the results to a file.
+
+    :param results: the text that will be written to the file
+    :param opt: 'w' for write, 'a' for append
+    :param report: the path to the PDF file
+    """
+    if report:
+        file_output = PARENT.joinpath(f"{os.path.basename(report).replace(' ', '_').replace('.pdf', '')}.txt")
         with open(file_output, opt, encoding="utf-8") as out:
             out.write(results)
 
@@ -47,94 +65,138 @@ class PDFWorker:
     def __init__(self):
         self.counter = 0
 
-    def processor(self, pdf_doc, output, title):
-        print(f"{tc.DOTSEP}\n{tc.GREEN} [ Gathering IOCs ]{tc.RESET}")
-        pages = list(extractor(pdf=pdf_doc))
+    def processor(self, pdfdoc: str, output: bool, title: str) -> None:
+        """
+        It takes a PDF document, extracts the text, and writes it to a file.
+
+        :param pdf_doc: The PDF document to be processed
+        :param output: The output directory
+        :param title: The title of the PDF document
+        """
+        print(f"{TC.DOTSEP}\n{TC.GREEN} [ Gathering IOCs ]{TC.RESET}")
+        pages = list(extractor(pdf=pdfdoc))
         try:
             text = "".join(filter(None, pages))
         except TypeError:
             print(f"Broken sentence: {''.join(filter(None, pages))}")
             raise
-        else:
-            # create output file
-            if output:
-                write_file(rep=title, results=f"\nTITLE: {title} \nPATH: {pdf_doc}\n", opt="w")
 
-            # Language detection
-            def lang_proc(selection):
-                if helper.lang_patts(text).get(selection):
-                    self.counter += 1
-                    spec = "".join(helper.lang_patts(text).get(selection))
-                    print(f"\n{tc.FOUND}{tc.BOLD}{selection}{tc.RESET}\n{tc.SEP}\n{spec}")
+        self.get_patterns(output, title, pdfdoc, text)
+
+    def get_patterns(self, output: bool, title: str, pdfdoc: str, text: str) -> None:
+        # sourcery skip: low-code-quality
+        """Create output file"""
+        if output:
+            write_file(report=title, results=f"\nTITLE: {title} \nPATH: {pdfdoc}\n", opt="w")
+
+        # Language detection
+        detected_language = HELPER.detect_language(text)
+        languages = ["ARABIC", "CYRILLIC", "CHINESE", "FARSI", "HEBREW"]
+        for language in languages:
+            if detected_language.get(language):
+                self.counter += 1
+                if spec := "".join(detected_language[language]):
+                    print(f"\n{TC.FOUND}{TC.BOLD}{language}{TC.RESET}\n{TC.SEP}\n{spec}")
                     if output:
-                        write_file(rep=title, results=f"\n{selection}\n{'-' * 15}\n{spec}", opt="a")
+                        write_file(report=title, results=f"\n{language}\n{'-' * 15}\n{spec}", opt="a")
 
-                    # remove from dict to avoid repeat pattern
-                    helper.lang_patts(text).pop(selection)
+        # Detect patterns
+        exclude = ("gov", "foo", "bar", "py")
+        for key, pattern in HELPER.patts(text).items():
+            if pattern:
+                self.counter += 1
+                sorted_patterns = sorted(set(pattern))
 
-            # Attempt to detect specific language characters
-            languages = ["ARABIC", "CYRILLIC", "CHINESE", "FARSI", "HEBREW"]
-            list(map(lang_proc, languages))
-
-            # Detect patterns
-            exclude = ("gov", "foo", "bar", "py")
-            for key, pattern in helper.patts(text).items():
-                if pattern:
-                    self.counter += 1
-                    sorted_set = sorted(set(pattern))
-
-                    if key == "DOMAIN":
-                        for domain in pattern:
-                            tld = domain.split(".")[-1]
-                            try:
-                                while not is_tld(tld) or tld in exclude:
-                                    sorted_set.remove(domain)
-                            except ValueError:
-                                pass
-
-                    pattern = "\n".join(sorted_set)
-                    print(f"\n{tc.FOUND}{tc.BOLD}{key}{tc.RESET}\n{tc.SEP}\n{pattern}")
-                    if output:
-                        write_file(rep=title, results=f"\n{key}\n{'-' * 15}\n{pattern}\n", opt="a")
-
-            if self.counter == 0:
-                print(f"{tc.YELLOW}= No IOCs found ={tc.RESET}")
+                if key == "DOMAIN":
+                    for domain in pattern:
+                        tld = domain.split(".")[-1]
+                        with contextlib.suppress(ValueError):
+                            while not is_tld(tld) or tld in exclude:
+                                sorted_patterns.remove(domain)
+                pattern = "\n".join(sorted_patterns)
+            if pattern:
+                print(f"\n{TC.FOUND}{TC.BOLD}{key}{TC.RESET}\n{TC.SEP}\n{pattern}")
                 if output:
-                    write_file(rep=title, results="= No IOCs found =", opt="w")
+                    write_file(report=title, results=f"\n{key}\n{'-' * 15}\n{pattern}\n", opt="a")
+
+        if self.counter == 0:
+            print(f"{TC.YELLOW}= No IOCs found ={TC.RESET}")
+            if output:
+                write_file(report=title, results="= No IOCs found =", opt="w")
+
+    # def get_patterns(self, output: bool, title: str, pdfdoc: str, text: str) -> None:
+    #     """Create output file"""
+    #     lang_results = ""
+    #     patts_results = ""
+
+    #     if output:
+    #         write_file(report=title, results=f"\nTITLE: {title} \nPATH: {pdfdoc}\n", opt="w")
+
+    #     # Language detection
+    #     detected_language = HELPER.detect_language(text)
+    #     languages = ["ARABIC", "CYRILLIC", "CHINESE", "FARSI", "HEBREW"]
+    #     for language in languages:
+    #         if detected_language.get(language):
+    #             self.counter += 1
+    #             if spec := "".join(detected_language[language]):
+    #                 lang_results += f"\n{language}\n{'-' * 15}\n{spec}\n"
+
+    #     # Detect patterns
+    #     exclude = ("gov", "foo", "bar", "py")
+    #     for key, pattern in HELPER.patts(text).items():
+    #         if pattern:
+    #             self.counter += 1
+    #             sorted_patterns = sorted(set(pattern))
+
+    #             if key == "DOMAIN":
+    #                 for domain in pattern:
+    #                     tld = domain.split(".")[-1]
+    #                     with contextlib.suppress(ValueError):
+    #                         while not is_tld(tld) or tld in exclude:
+    #                             sorted_patterns.remove(domain)
+    #             pattern = "\n".join(sorted_patterns)
+    #         if pattern:
+    #             patts_results += f"\n{key}\n{'-' * 15}\n{pattern}\n"
+
+    #     if lang_results or patts_results:
+    #         if lang_results:
+    #             print(f"\n{TC.FOUND}LANGUAGES{TC.RESET}\n{lang_results}")
+    #         if patts_results:
+    #             print(f"\n{TC.FOUND}PATTERNS{TC.RESET}\n{patts_results}")
+    #         if output:
+    #             write_file(report=title, results=lang_results + patts_results, opt="a")
+    #     else:
+    #         print(f"{TC.YELLOW}= No IOCs found ={TC.RESET}")
+    #         if output:
+    #             write_file(report=title, results="= No IOCs found =", opt="w")
 
 
 def main():
+    """
+    Main program
+    """
     parser = argparse.ArgumentParser(description="PDF IOC Extractor")
     parser.add_argument(dest="pdf_doc", help="Path to single PDF document")
     parser.add_argument("-o", "--out", dest="output", action="store_true", help="Write output to file")
     args = parser.parse_args()
 
-    if len(sys.argv[1:]) == 0:
-        parser.print_help()
-        parser.exit()
+    if not Path(args.pdf_doc).exists():
+        raise SystemExit(f"{TC.RED}[ERROR]{TC.RESET} No such file: {args.pdf_doc}")
 
-    if not Path(args.pdf_doc).is_file():
-        raise SystemExit(f"{tc.RED}[ERROR]{tc.RESET} No such file: {args.pdf_doc}")
-
-    title = ""
-    if "\\" in args.pdf_doc:
-        title = args.pdf_doc.split("\\")[-1]
-    else:
-        title = args.pdf_doc.split("/")[-1]
+    title = os.path.basename(args.pdf_doc)
     worker = PDFWorker()
-    worker.processor(pdf_doc=args.pdf_doc, output=args.output, title=title)
+    worker.processor(pdfdoc=args.pdf_doc, output=args.output, title=title)
 
 
 if __name__ == "__main__":
-    banner = rf"""
+    BANNER = r"""
         ____     ____   ______
        / __ \   /  _/  / ____/
       / /_/ /   / /   / __/
      / ____/  _/ /   / /___
     /_/      /___/  /_____/
 
-    PDF IOC Extractor {__version__}
+    PDF IOC Extractor
     """
-
-    print(f"{tc.CYAN}{banner}{tc.RESET}")
+    print(f"{TC.CYAN}{BANNER}{TC.RESET}")
     main()
--- utils.py
+++ utils.py
@@ -2,105 +2,144 @@ import re
 
 from colorama import Fore, Style, init
 
-__author__ = "DFIRSec (@pulsecode)"
-__description__ = "Extract Indicators of Compromise (IOCs) from PDF documents."
-
 
 class Helpers:
-    @staticmethod
-    def regex(_type):
-        pattern = dict(
-            # languages
-            arabic=r"[\u0600-\u06FF\u0698\u067E\u0686\u06AF]",
-            chinese=r"[\u4E00-\u9FFF]",
-            cyrillic=r"[\u0400-\u04FF]",
-            hebrew=r"[\u0590-\u05FF]",
-            # hashes
-            md5=r"\b[A-Fa-f0-9]{32}\b",
-            sha1=r"\b[A-Fa-f0-9]{40}\b",
-            sha256=r"\b[A-Fa-f0-9]{64}\b",
-            sha512=r"\b[A-Fa-f0-9]{128}\b",
-            # web-related
-            # domain=r"([A-Za-z0-9]+(?:[\-|\.|][A-Za-z0-9]+)*(?<!fireeye)(?:\[\.\]|\.)(?!["
-            # r"a-z-]*.\.add|ako|area|argv|asn|asp|bar|bat|bak|bin|bmp|btz|cfg|cfm|class|cpj|conf|copy|dat|db|dll"
-            # r"|dis|dns|doc|div|drv|dx|err|exe|file|foo|get|gif|gov|gz|hta|htm|http|img|inf|ini|jar|java|jsp|jpg"
-            # r"|key|lnk|log|md|min|msi|mtx|mul|nat|name|rar|rer|rpm|out|pack|pcap|pdf|php|pop|png|ps|put|py|src"
-            # r"|sh|sort|sys|tmp|txt|user|vbe|vbs|xls|xml|xpm|zip|[i\.e]$|[e\.g]$)(?:[a-z]{2,4})\b|(?:\[\.\][a-z]{"
-            # r"2,4})(?!@)$)",
-            domain=r"([A-Za-z0-9]+(?:[\-|\.|][A-Za-z0-9]+)*(?<!fireeye)(?:\[\.\]|\.)(?![a-z-]*.[i\.e]$|[e\.g]$)(?:[a-z]{2,4})\b|(?:\[\.\][a-z]{2,4})(?!@)$)",
-            email=r"([a-zA-Z0-9_.+-]+(\[@\]|@)(?!fireeye)[a-zA-Z0-9-.]+(\.|\[\.\])(?![a-z-]+\.gov|gov)([a-zA-Z0-9-.]{"
-            r"2,6}\b))",
-            ipv4=r"(((?![0])(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\[\.\]|\.))){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9]["
-            r"0-9]?)",
-            url=r"(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=\*]*))",
-            webfile=r"(([^\s|\d\W*])+[a-z-A-Z0-9\-\_ ]+(\.|\[\.\])(hta|html|htm|htmls|java|jsp|js|php|asp$|aspx))",
-            # file-related
-            archive=r"(([^\s|\d\W*])+[a-z-A-Z0-9\-\_ ]+(\.|\[\.\])(zip|7z|jar|gz|rar|xz|tar|tar\.gz))",
-            binary=r"(([^\s|\W])+([a-z-A-Z0-9\-\_]|[\u4E00-\u9FFF]|[\u0400-\u04FF])+((?:\.exe)|(?:\.msi)|(?:\.dll)|("
-            r"?:\.bin)))",
-            env_var=r"(\%+[a-zA-Z0-9]+\%.*[^\"])",
-            image=r"(([^\s|\d\W*])+[a-z-A-Z0-9\-\_ ]+(\.|\[\.\])(bmp|gif|jpg|jpeg|png|svg|tiff|wepb))",
-            misc_file=r"(([^\s|\W])+([a-z-A-Z0-9\-\_])+((?:\.txt)|(?:\.csv)))",
-            office=r"(([^\s|\d\W*])+[a-z-A-Z0-9\-\_ ]+\.(doc|docx|xls|xlsx|pdf))",
-            script=r"(([^\s|(\"])+[a-z-A-Z0-9\-\_]+((?:\.vbs)|(?:\.sh)|(?:\.bat)|(?:\.ps1)|(?:\.py)))",
-            windir=r"([a-zA-Z]{1}:(\\|\\\\|\/\/)(?<![a-zA-Z]:\/\/)[a-zA-Z0-9\-\_\\\/].+([^\s|\"]+)[^\.\"\r\n])",
-            # misc
-            btc=r"(^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$)",
-            base64=r"(^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$)",
-            # pii
-            address=r"(^(\d+)\s?([A-Za-z](?=\s))?\s(.*?)\s([^ ]+?)\s?((?<=\s)APT)?\s?((?<=\s)\d*)?$)",
-            cc=r"((?:(?:\\d{4}[- ]?){3}\\d{4}|\\d{15,16}))(?![\\d])",
-            date=r"(?:(?<!\:)(?<!\:\d)[0-3]?\d(?:st|nd|rd|th)?\s+(?:of\s+)?("
-            r"?:jan\.?|january|feb\.?|february|mar\.?|march|apr\.?|april|may|jun\.?|june|jul\.?|july|aug"
-            r"\.?|august|sep\.?|september|oct\.?|october|nov\.?|november|dec\.?|december)|("
-            r"?:jan\.?|january|feb\.?|february|mar\.?|march|apr\.?|april|may|jun\.?|june|jul\.?|july|aug"
-            r"\.?|august|sep\.?|september|oct\.?|october|nov\.?|november|dec\.?|december)\s+(?<!\:)(?<!\:\d)["
-            r"0-3]?\d(?:st|nd|rd|th)?)(?:\,)?\s*(?:\d{4})?|[0-3]?\d[-\./][0-3]?\d[-\./]\d{2,4}",
-            phone=r"(^(?:(?<![\d-])(?:\+?\d{1,3}[-.\s*]?)?(?:\(?\d{3}\)?[-.\s*]?)?\d{3}[-.\s*]?\d{4}(?![\d-]))|(?:("
-            r"?<![\d-])(?:(?:\(\+?\d{2}\))|(?:\+?\d{2}))\s*\d{2}\s*\d{3}\s*\d{4}(?![\d-]))\$)",
-            po_box=r"P\.? ?O\.? Box \d+",
-            ssn=r"(?!000|666|333)0*(?:[0-6][0-9][0-9]|[0-7][0-6][0-9]|[0-7][0-7][0-2])[- ](?!00)[0-9]{2}[- ](?!0000)["
-            r"0-9]{4}",
-            zip_code=r"\b\d{5}(?:[-\s]\d{4})?\b",
-        )
-        return re.compile(pattern[_type])
+    """Helper functions for the program"""
 
-    @staticmethod
-    def reiter(regex, text):
-        return [x.group() for x in re.finditer(regex, text.lower())]
+    def __init__(self):
+        pass
 
-    def lang_patts(self, text):
+    def regex(self, category: str, pattern_name: str):  # sourcery skip: docstrings-for-functions
+        patterns = {
+            "languages": {
+                "arabic": re.compile(r"[\u0600-\u06FF\u0698\u067E\u0686\u06AF]"),
+                "chinese": re.compile(r"[\u4E00-\u9FFF]"),
+                "cyrillic": re.compile(r"[\u0400-\u04FF]"),
+                # "hebrew": re.compile(r"[\u0590-\u05FF]"),
+                "hebrew": re.compile(r"[\u0590-\u05FF\uFB2A-\uFB4E]")
+            },
+            "hashes": {
+                "md5": re.compile(r"\b[A-Fa-f0-9]{32}\b"),
+                "sha1": re.compile(r"\b[A-Fa-f0-9]{40}\b"),
+                "sha256": re.compile(r"\b[A-Fa-f0-9]{64}\b"),
+                "sha512": re.compile(r"\b[A-Fa-f0-9]{128}\b"),
+            },
+            "web-related": {
+                "domain": re.compile(
+                    r"([A-Za-z0-9]+(?:[\-|\.|][A-Za-z0-9]+)*(?:\[\.\]|\.)(?![a-z-]*.[i\.e]$"
+                    r"|[e\.g]$)(?:[a-z]"
+                    r"{2,4})\b|(?:\[\.\][a-z]{2,4})(?!@)$)"
+                ),
+                "email": re.compile(
+                    r"([a-zA-Z0-9_.+-]+(\[@\]|@)(?!fireeye)[a-zA-Z0-9-.]+(\.|\[\.\])(?![a-z-]+\.gov|gov)"
+                    r"([a-zA-Z0-9-.]{2,6}\b))"
+                ),
+                "ipv4": re.compile(
+                    r"(((?![0])(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\[\.\]|\.))){3}(25[0-5]|"
+                    r"2[0-4][0-9]|[01]?[0-9][0-9]?)"
+                ),
+                "url": re.compile(
+                    r"(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]"
+                    r"{1,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=\*]*))"
+                ),
+                "webfile": re.compile(
+                    r"(([^\s|\d\W*])+[a-z-A-Z0-9\-\_ ]+(\.|\[\.\])(hta|html|htm|htmls|java|jsp|js|php|asp$|aspx))"
+                ),
+            },
+            "file-related": {
+                "archive": re.compile(
+                    r"(([^\s|\d\W*])+[a-z-A-Z0-9\-\_ ]+(\.|\[\.\])(zip|7z|jar|gz|rar|xz|tar|tar\.gz))"
+                ),
+                "binary": re.compile(
+                    r"(([^\s|\W])+([a-z-A-Z0-9\-\_]|[\u4E00-\u9FFF]|[\u0400-\u04FF])+((?:\.exe)|"
+                    r"(?:\.msi)|(?:\.dll)|(?:\.bin)))"
+                ),
+                "env_var": re.compile(r"(\%+[a-zA-Z0-9]+\%.*[^\"])"),
+                "image": re.compile(
+                    r"(([^\s|\d\W*])+[a-z-A-Z0-9\-\_ ]+(\.|\[\.\])(bmp|gif|jpg|jpeg|png|svg|tiff|wepb))"
+                ),
+                "misc_file": re.compile(r"(([^\s|\W])+([a-z-A-Z0-9\-\_])+((?:\.txt)|(?:\.csv)))"),
+                "office": re.compile(r"(([^\s|\d\W*])+[a-z-A-Z0-9\-\_ ]+\.(doc|docx|xls|xlsx|pdf))"),
+                "script": re.compile(
+                    r"(([^\s|(\"])+[a-z-A-Z0-9\-\_]+((?:\.vbs)|(?:\.sh)|(?:\.bat)|(?:\.ps1)|(?:\.py)))"
+                ),
+                "windir": re.compile(
+                    r"([a-zA-Z]{1}:(\\|\\\\|\/\/)(?<![a-zA-Z]:\/\/)[a-zA-Z0-9\-\_\\\/].+([^\s|\"]+)[^\.\"\r\n])"
+                ),
+            },
+            "misc": {
+                # "btc": re.compile(r"^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$)"),
+                "base64": re.compile(
+                    r"(^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$)"
+                ),
+            },
+            "pii": {
+                "address": re.compile(
+                    r"(^(\d+)\s?([A-Za-z](?=\s))?\s(.*?)\s([^ ]+?)\s?((?<=\s)APT)?\s?((?<=\s)\d*)?$)"
+                ),
+                # "cc": re.compile(r"((?:(?:\\d{4}[- ]?){3}\\d{4}|\\d{15,16}))(?![\\d]))"),
+                "date": re.compile(
+                    r"(?:(?<!\:)(?<!\:\d)[0-3]?\d(?:st|nd|rd|th)?\s+(?:of\s+)?("
+                    r"?:jan\.?|january|feb\.?|february|mar\.?|march|apr\.?|april|may|jun\.?|june|jul\.?|july|aug"
+                    r"\.?|august|sep\.?|september|oct\.?|october|nov\.?|november|dec\.?|december)|("
+                    r"?:jan\.?|january|feb\.?|february|mar\.?|march|apr\.?|april|may|jun\.?|june|jul\.?|july|aug"
+                    r"\.?|august|sep\.?|september|oct\.?|october|nov\.?|november|dec\.?|december)\s+(?<!\:)(?<!\:\d)["
+                    r"0-3]?\d(?:st|nd|rd|th)?)(?:\,)?\s*(?:\d{4})?|[0-3]?\d[-\./][0-3]?\d[-\./]\d{2,4}"
+                ),
+                "phone": re.compile(
+                    r"(^(?:(?<![\d-])(?:\+?\d{1,3}[-.\s*]?)?(?:\(?\d{3}\)?[-.\s*]?)?\d{3}[-.\s*]"
+                    r"?\d{4}(?![\d-]))|(?:("
+                    r"?<![\d-])(?:(?:\(\+?\d{2}\))|(?:\+?\d{2}))\s*\d{2}\s*\d{3}\s*\d{4}(?![\d-]))\$)"
+                ),
+                "po_box": re.compile(r"P\.? ?O\.? Box \d+"),
+                # "ssn": re.compile(
+                #     r"(?!000|666|333)0*(?:[0-6][0-9][0-9]|[0-7][0-6][0-9]|[0-7][0-7][0-2])[- ](?!00))"
+                #     r"[0-9]{2}[- ](?!0000)[0-9]{4}"
+                # ),
+                "zip_code": re.compile(r"\b\d{5}(?:[-\s]\d{4})?\b"),
+            },
+        }
+        return patterns[category][pattern_name]
+
+    def reiter(self, regex: re.Pattern[str], text: str):  # sourcery skip: docstrings-for-functions
+        return (x.group() for x in re.finditer(regex, text.lower()))
+
+    def detect_language(self, text: str):  # sourcery skip: docstrings-for-functions
         return {
-            "ARABIC": self.reiter(self.regex(_type="arabic"), text),
-            "CHINESE": self.reiter(self.regex(_type="chinese"), text),
-            "CYRILLIC": self.reiter(self.regex(_type="cyrillic"), text),
-            "HEBREW": self.reiter(self.regex(_type="hebrew"), text),
+            "ARABIC": self.reiter(self.regex("languages", "arabic"), text),
+            "CHINESE": self.reiter(self.regex("languages", "chinese"), text),
+            "CYRILLIC": self.reiter(self.regex("languages", "cyrillic"), text),
+            "HEBREW": self.reiter(self.regex("languages", "hebrew"), text),
         }
 
-    def patts(self, text):
+    def patts(self, text: str):  # sourcery skip: docstrings-for-functions
         return {
-            "ARCHIVE": self.reiter(self.regex(_type="archive"), text),
-            "BINARY": self.reiter(self.regex(_type="binary"), text),
-            "BTC": self.reiter(self.regex(_type="btc"), text),
-            "DOMAIN": self.reiter(self.regex(_type="domain"), text),
-            "EMAIL": self.reiter(self.regex(_type="email"), text),
-            "ENVIRONMENT VARIABLE": self.reiter(self.regex(_type="env_var"), text),
-            "MISC FILE": self.reiter(self.regex(_type="misc_file"), text),
-            "IMAGE": self.reiter(self.regex(_type="image"), text),
-            "IPV4": self.reiter(self.regex(_type="ipv4"), text),
-            "MD5": self.reiter(self.regex(_type="md5"), text),
-            "OFFICE/PDF": self.reiter(self.regex(_type="office"), text),
-            "PHONE": self.reiter(self.regex(_type="phone"), text),
-            "SCRIPT": self.reiter(self.regex(_type="script"), text),
-            "SHA1": self.reiter(self.regex(_type="sha1"), text),
-            "SHA256": self.reiter(self.regex(_type="sha256"), text),
-            "URL": self.reiter(self.regex(_type="url"), text),
-            "WEB FILE": self.reiter(self.regex(_type="webfile"), text),
-            "WIN DIR": self.reiter(self.regex(_type="windir"), text),
+            "ARCHIVE": self.reiter(self.regex("hashes", "md5"), text),
+            "BINARY": self.reiter(self.regex("hashes", "sha1"), text),
+            "BTC": self.reiter(self.regex("hashes", "sha256"), text),
+            "DOMAIN": self.reiter(self.regex("web-related", "domain"), text),
+            "EMAIL": self.reiter(self.regex("web-related", "email"), text),
+            "ENVIRONMENT VARIABLE": self.reiter(self.regex("file-related", "env_var"), text),
+            "MISC FILE": self.reiter(self.regex("file-related", "misc_file"), text),
+            "IMAGE": self.reiter(self.regex("file-related", "image"), text),
+            "IPV4": self.reiter(self.regex("web-related", "ipv4"), text),
+            "MD5": self.reiter(self.regex("hashes", "md5"), text),
+            "OFFICE/PDF": self.reiter(self.regex("file-related", "office"), text),
+            "PHONE": self.reiter(self.regex("pii", "phone"), text),
+            "SCRIPT": self.reiter(self.regex("file-related", "script"), text),
+            "SHA1": self.reiter(self.regex("hashes", "sha1"), text),
+            "SHA256": self.reiter(self.regex("hashes", "sha256"), text),
+            "URL": self.reiter(self.regex("web-related", "url"), text),
+            "WEB FILE": self.reiter(self.regex("web-related", "webfile"), text),
+            "WIN DIR": self.reiter(self.regex("file-related", "windir"), text),
         }
 
 
 class Termcolors:
+    """
+    Color constants used to colorize text in the terminal.
+    """
+
     # Initialize colorama
     init()
 
