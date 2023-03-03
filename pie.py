e8b49cf709a2ae2055b2e22c8b9e3bf7ed907901
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2023-03-03 08:52:46 -0500

    Refactored

--- pie.py
+++ pie.py
@@ -1,27 +1,23 @@
 """
-Takes a PDF document as an argument, extracts the text from the PDF, and then searches the text for IOCs.
+Extracts IOCs from PDF File.
 """
 import argparse
-import contextlib
 import os
 import sys
+from datetime import datetime, timedelta
 from pathlib import Path
 from typing import Optional
 
 import pdfplumber
-from tld import is_tld
-from tld.utils import update_tld_names
+import requests
 from utils import Helpers, Termcolors
 
 __author__ = "DFIRSec (@pulsecode)"
-__version__ = "v0.0.9"
+__version__ = "v0.1.0"
 __description__ = "Extract Indicators of Compromise (IOCs) from PDF documents."
 
 HELPER = Helpers()
 TC = Termcolors()
-
-# update/sync tld names
-update_tld_names()
 
 # Base directory
 PARENT = Path(__file__).resolve().parent
@@ -76,7 +72,8 @@ class PDFWorker:
         :param output: The output directory
         :param title: The title of the PDF document
         """
-        print(f"{TC.DOTSEP}\n{TC.GREEN} [ Gathering IOCs ]{TC.RESET}")
+        print(f"{TC.GREEN}\n[ Gathering IOCs ]{TC.RESET}\n{TC.DOTSEP}")
+
         pages = list(extractor(pdf=pdfdoc))
         try:
             text = "".join(filter(None, pages))
@@ -87,12 +84,12 @@ class PDFWorker:
         self.get_patterns(output, title, pdfdoc, text)
 
     def get_patterns(self, output: bool, title: str, pdfdoc: str, text: str) -> None:
-        # sourcery skip: low-code-quality, use-named-expression
+        # sourcery skip: low-code-quality
         """Create output file"""
         if output:
             write_file(report=title, results=f"\nTITLE: {title} \nPATH: {pdfdoc}\n", opt="w")
 
-        # Language detection
+        # language detection
         detected_language = HELPER.detect_language(text)
         languages = ["ARABIC", "CYRILLIC", "CHINESE", "FARSI", "HEBREW"]
         for language in languages:
@@ -103,24 +100,39 @@ class PDFWorker:
                     if output:
                         write_file(report=title, results=f"\n{language}\n{'-' * 15}\n{spec}", opt="a")
 
-        # Detect patterns
-        exclude = ("gov", "foo", "bar", "py")
+        # excluded tlds
+        exclude = ("gov", "foo")
+
+        # check if the tlds file is present and up to date
+        self.download_tlds()
+
+        # create a set of valid tlds
+        valid_tlds = set()
+        tlds_file = "tlds-alpha-by-domain.txt"
+        with open(tlds_file, encoding="utf-8") as fileobj:
+            for line in fileobj:
+                tld = line.strip().lower()
+                if tld and not tld.startswith("#"):
+                    valid_tlds.add(tld)
+
+        # get all patterns
         for key, pvals in HELPER.patts(text).items():
             if pvals:
                 sorted_patterns = sorted(set(pvals))
                 if sorted_patterns:
                     self.counter += 1
 
+                # check if domain tld is in the valid tlds
                 if key == "DOMAIN":
                     new_patterns = set()
                     for domain in sorted_patterns:
-                        tld = domain.split(".")[-1]
-                        if is_tld(tld) and tld not in exclude:
+                        tld = domain.split(".")[-1].lower()
+                        if tld in valid_tlds and tld not in exclude:
                             new_patterns.add(domain)
                         sorted_patterns = new_patterns
 
-                pattern = "\n".join(sorted_patterns)
-                if pattern:
+                # return the sorted patterns
+                if pattern := "\n".join(sorted_patterns):
                     print(f"\n{TC.FOUND}{TC.BOLD}{key}{TC.RESET}\n{TC.SEP}\n{pattern}")
                     if output:
                         write_file(report=title, results=f"\n{key}\n{'-' * 15}\n{pattern}\n", opt="a")
@@ -129,6 +141,57 @@ class PDFWorker:
             print(f"{TC.YELLOW}= No IOCs found ={TC.RESET}")
             if output:
                 write_file(report=title, results="= No IOCs found =", opt="w")
+
+    def download_tlds(self, age_limit_days: int = 3):
+        """
+        Downloads TLDS file, saves it to a file, and returns the path to the file
+
+        :param url: The URL of the file to download
+        :param filename: The name of the file to download
+        :param age_limit_days: The number of days that the file can be old before it is updated, defaults to
+        3 (optional)
+        :return: The file path of the downloaded file.
+        """
+        url = "https://data.iana.org/TLD/tlds-alpha-by-domain.txt"
+        filename = "tlds-alpha-by-domain.txt"
+        filepath = Path(filename)
+
+        try:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as err:
+            print(f"Error downloading file: {err}")
+            return None
+
+        if filepath.exists():
+            mtime = filepath.stat().st_mtime
+            mtime_datetime = datetime.fromtimestamp(mtime)
+
+            now = datetime.now()
+            delta = now - mtime_datetime
+            age_limit = timedelta(days=age_limit_days)
+
+            if delta > age_limit:
+                print(f"TLDS file is older than {age_limit_days} days, updating...")
+                try:
+                    with open(filepath, "wb") as fileobj:
+                        fileobj.write(response.content)
+                except Exception as err:
+                    print(f"Error updating file: {err}")
+                    return None
+
+        else:
+            print("[!] The TLDS file is missing, downloading file...\n")
+            try:
+                with open(filepath, "wb") as fileobj:
+                    fileobj.write(response.content)
+            except Exception as err:
+                print(f"Error downloading file: {err}")
+                return None
+
+            print(f"[+] The file {filename} has been downloaded and saved.")
+
+        return str(filepath.resolve())
 
 
 def main():
@@ -159,4 +222,5 @@ if __name__ == "__main__":
     PDF IOC Extractor
     """
     print(f"{TC.CYAN}{BANNER}{TC.RESET}")
+
     main()
